b980d8e23df9d9effb07fe7ff7ea56d4d570d3ee
Timmir0 <124453971+Timmir0@users.noreply.github.com>
2023-04-26 23:00:20 -0400

    Quick update to stuff

--- Pinball Eternal Game/Assets/Scripts/Player Scripts/PlayerShoot.cs
+++ Pinball Eternal Game/Assets/Scripts/Player Scripts/PlayerShoot.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
+    bool isFireCD = false;
+    [SerializeField] float fireCooldown = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,15 @@ public class PlayerShoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1") && !isFireCD)
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        //shoots a raycast, checking to see if it's an enemy and within range, then activates the appropriate
+        //knockback script
     }
 }
